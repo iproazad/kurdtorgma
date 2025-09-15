ba2dda1c97711b7952d60f6e98839c9ab72c153a
iproazad <125834441+iproazad@users.noreply.github.com>
2025-09-15 20:35:47 +0300

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -1,5 +1,4 @@
 
-
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideZonelessChangeDetection } from '@angular/core';
 import { AppComponent } from './src/app.component';
